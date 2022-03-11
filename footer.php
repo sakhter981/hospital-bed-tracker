3c0e6b9a097ed970420a6e8413ff62735ad6e29d
shoaib akhter <sakhter981@gmail.com>
2022-03-11 12:30:56 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
         </div>	
         <!-- // #containerHolder -->
         
-        <p id="footer">Hospital Bed Tracking System. &copy; 2022. All Rights Reserved - Ritik katiyar</p>
+        <p id="footer">Hospital Bed Tracking System. &copy; 2022. All Rights Reserved - Team ALPHA</p>
     </div>
     <!-- // #wrapper -->
 </body>
