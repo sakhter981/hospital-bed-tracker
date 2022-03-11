9901eae2fa74c7638ec845dc2b2ec2f884973da0
Ritik katiyar <55633948+ritikkatiyar@users.noreply.github.com>
2022-03-11 11:01:23 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,12 @@
+
+                <div class="clear"></div>
+            </div>
+            <!-- // #container -->
+        </div>	
+        <!-- // #containerHolder -->
+        
+        <p id="footer">Hospital Bed Tracking System. &copy; 2022. All Rights Reserved - Ritik katiyar</p>
+    </div>
+    <!-- // #wrapper -->
+</body>
+</html>
